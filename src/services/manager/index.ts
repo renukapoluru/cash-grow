83d7b071ef887f69fd622dd541347cb864897a56
polurur <polurur@nova.tech>
2021-08-26 14:18:10 +0530

    adding more fields

--- src/services/manager/index.ts
+++ src/services/manager/index.ts
@@ -1,18 +1,25 @@
 
 import axios from 'axios';
 
-const fusionBaseUrl = 'https://fusion.preprod.zeta.in/api/v1/ifi/140793/';
+const fusionBaseUrl = 'https://fusion.preprod.zeta.in/api/v1/ifi/140793';
 
 const userToken = 'eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwidGFnIjoiQVJydlQzRzBZRjl1clRiN08zdXo5USIsImFsZyI6IkExMjhHQ01LVyIsIml2IjoiVnItNDdKVFdPZW9MVnp3eiJ9.J84KJiobSaTW789Uxva_2OsqryjbyMSRnnJTCynU1nI.r2QeAfegTPMkWdvrFobIbA.H04PspJi921p-NTGLe7UwRzeKZY1s9rVwqdAZVIpDLvUw8Lo1T9JwrxtRws_MkOOGYKb7UZ21lnPBxpxzq8PmDyKhFGH8vODnTpkyDl1UsxCJ6th5f8htgKmQK6efXCunHWkmtap8apqrxoGCZzo_RVC-tDhs13Fngh3a-XTHCXPsi45RmqX66AV_ujWl8_DfAxwnsl5v3oYkJAl5yaqmrbjtNJjGyIO84Em-KeN5ZYPyDllDDpRdBnTw_68XCxYju_jgRDMPxW0a2opJkurigj3s5v9kDMVhkWiSJY6UzVnxErywPtOX0WGxL1Zh4bGqP00qA1zUnwxWm6KZJb5j33GUnBEeiZq53xaoEHQZqQsc338FRQZjljeu5kfu0pp.6tIJinfmsIdoyUGMImRdCQ';
 
+const headers = {
+    headers: {
+        'X-Zeta-AuthToken' : `${userToken}`
+    }
+};
 export class CashGrowService {
+
+    createAccountHolder(data: any) {
+        const url = `${fusionBaseUrl}/applications/newIndividual`;
+        return axios.post(url, JSON.stringify(data),headers);
+    }
+
     getAccountDetails(id: string)
     {
         const url = `${fusionBaseUrl}/accounts/${id}`;
-        return axios.get(url,{
-            headers: {
-                'X-Zeta-AuthToken' : `${userToken}`
-            }
-        });
+        return axios.get(url,headers);
     }
 }
--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -16,8 +16,25 @@
             </select>
           </div>
           <div class="input-field">
+            <img class="input-icon" :src="genderIcon" />
+            <select v-model="gender" name="role" id="role">
+              <option value="Select" selected disabled>Select Gender</option>
+              <option value="MALE">Male</option>
+              <option value="FEMALE">Female</option>
+              <option value="OTHERS">Others</option>
+            </select>
+          </div>
+          <div class="input-field">
             <img class="input-icon" :src="userIcon" />
-            <input v-model="name" type="text" placeholder="Full Name" />
+            <input v-model="firstname" type="text" placeholder="First Name" />
+          </div>
+          <div class="input-field">
+            <img class="input-icon" :src="userIcon" />
+            <input v-model="lastame" type="text" placeholder="Last Name" />
+          </div>
+          <div class="input-field">
+            <img class="input-icon" :src="dobIcon" />
+            <input v-model="dob" type="date">
           </div>
           <div class="input-field">
             <img class="input-icon" :src="emailIcon" />
@@ -49,6 +66,11 @@
 import { IonPage, IonContent, toastController } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
+import { uniqueId } from '@/common/utils';
+
+import { CashGrowManager } from "@/services/services";
+
+
 export default  defineComponent({
   name: 'SignUp',
   components: { 
@@ -57,11 +79,14 @@ export default  defineComponent({
   },
   data(){
     return{
-      name: '',
+      firstname: '',
+      lastname: '',
+      gender: 'Select',
       role: 'I want to..',
       password: '',
       repassword: '',
       email: '',
+      dob: '',
       phone: '',
       usersIcon : require('@/assets/users-icon.png'),
       userIcon : require('@/assets/user-icon.png'),
@@ -69,40 +94,97 @@ export default  defineComponent({
       borrowIcon: require('@/assets/borrow-icon.png'),
       emailIcon: require('@/assets/email-icon.png'),
       callIcon: require('@/assets/call-icon.png'),
-      passwordIcon: require('@/assets/password-icon.png')
+      passwordIcon: require('@/assets/password-icon.png'),
+      genderIcon: require('@/assets/bigender.png'),
+      dobIcon: require('@/assets/calendar-icon.png')
     }
   },
   methods:{
         async signUp(){
-           //const response = await axios.get("https://jsonplaceholder.typicode.com/todos");
-           console.log(this.role);           
-          const rndInt = Math.floor(Math.random() * 12) + 1;
-          const limit = rndInt*500*1000;
-          const data = {
-            name: this.name,
-            role: this.role,
-            email: this.email,
-            userId: this.createUserId(),
-            password: this.password,
-            limit: limit
+          const dt = new Date(this.dob);
+          const year = dt.getFullYear();
+          const month =  parseInt((dt.getMonth() < 10 ? '0' : '') + (dt.getMonth()+1));
+          const day = parseInt((dt.getDate() < 10 ? '0' : '') + dt.getDate());
+          const accountHolder = {
+              "ifiID": "140793",
+              "formID": uniqueId(),
+              "spoolID": "123",
+              "individualType": "REAL",
+              "salutation": "Ms.",
+              "firstName": this.firstname,
+              "middleName": "",
+              "lastName": this.lastname,
+              "profilePicURL": "https://i.im.ge/2021/08/26/QQqB39.jpg",
+              "dob": {
+                  "year": year,
+                  "month": month,
+                  "day": day
+              },
+              "gender": this.gender,
+              "mothersMaidenName": "",
+              "kycDetails": {
+                  "kycStatus": "MINIMAL",
+                  "kycStatusPostExpiry": "string",
+                  "kycAttributes": {},
+                  "authData": {
+                      "PAN": ""
+                  },
+                  "authType": "PAN"
+              },
+              "vectors": [
+                  {
+                      "type": "p",
+                      "value": `+91${this.phone}`,
+                      "isVerified": false
+                  }
+              ],
+              "pops": [],
+              "customFields": {
+                  "companyID": [
+                      1,
+                      2,
+                      3
+                  ]
+              },
+              "tags": [
+                  {
+                      "type": "vbo",
+                      "value": "swiggy",
+                      "isVerified": false
+                  }
+              ],
+              "source": "postman"
           };
-          fetch('https://6107b8f1d73c6400170d35a9.mockapi.io/users', {
-            method: 'POST', // or 'PUT'
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data),
-          })
-          .then(response => response.json())
-          .then(data => {
-            console.log('Success:', data);
-            this.callToast('Account created. Please sign in.');
-            this.$router.push('/signin');
-          })
-          .catch((error) => {
-            console.error('Error:', error);
-            this.callToast('Error creating account.');
-          });
+          console.log('Account Holder Body', accountHolder);
+          CashGrowManager.createAccountHolder(accountHolder);
+          // const rndInt = Math.floor(Math.random() * 12) + 1;
+          // const limit = rndInt*500*1000;
+          // const data = {
+          //   firstname: this.firstname,
+          //   lastname: this.lastname,
+          //   role: this.role,
+          //   email: this.email,
+          //   userId: this.createUserId(),
+          //   password: this.password,
+          //   limit: limit
+          // };
+          // fetch('https://6107b8f1d73c6400170d35a9.mockapi.io/users', {
+          //   method: 'POST', // or 'PUT'
+          //   headers: {
+          //     'Content-Type': 'application/json',
+          //   },
+          //   body: JSON.stringify(data),
+          // })
+          // .then(response => response.json())
+          // .then(data => {
+          //   console.log('Success:', data);
+          //   this.callToast('Account created. Please sign in.');
+          //   this.$router.push('/signin');
+          // })
+          // .catch((error) => {
+          //   console.error('Error:', error);
+          //   this.callToast('Error creating account.');
+          // });
         },
         createUserId() {
           let ID = "";
